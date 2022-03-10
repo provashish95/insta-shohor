f4414424bf782640de4bfbeddb6a69ef6df3c84c
Provashish Roy <provashishroy95@gmail.com>
2022-03-11 00:13:49 +0600

    navbar like  button worked and showing all liked posts

--- js/app.js
+++ js/app.js
@@ -143,7 +143,9 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById("liked").innerHTML = '';
   const likedPosts = getLikedPosts();
+  console.log(likedPosts);
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
